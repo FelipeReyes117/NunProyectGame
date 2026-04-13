01577e508b0c0c0f240a820030f4d38865ec18ce
MotkaZinov <128003046+MotkaZinov@users.noreply.github.com>
2026-04-13 08:09:34 -0700

    Actualizacion de menu
    Se hizo la programacion de los botones y se agregaron los scripts

--- NunGameProyect/NunProjectGame/Assets/Scripts/MenuScripts.cs
+++ NunGameProyect/NunProjectGame/Assets/Scripts/MenuScripts.cs
@@ -5,17 +5,26 @@ public class MenuScripts : MonoBehaviour
 {
     public GameObject OptionsMenu;
     public GameObject MainMenu;
-    
+    public GameObject BackToGame;
+    public GameObject BackToMainMenu;
     public void OpenOptionsPanel()
     {
     	MainMenu.SetActive(false);
     	OptionsMenu.SetActive(true);
     }
-    
+    public void ResumeGame()
+    {
+         Debug.Log("En el juego");
+    }
     public void OpenMainMenuPanel()
     {
     	MainMenu.SetActive(true);
     	OptionsMenu.SetActive(false);
+    }
+    public void OpenMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+        Debug.Log("Back to menu");
     }
     
     public void QuitGame()
