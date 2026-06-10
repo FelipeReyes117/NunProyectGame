24eb9f42856dfd64f2724367efddc0132ee2cad4
MotkaZinov <128003046+MotkaZinov@users.noreply.github.com>
2026-06-09 23:01:43 -0700

    Cambios en luces y pantallas de transicion

---
+++ NunGameProyect/Assets/scripts/MenuSystem.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuSystem : MonoBehaviour
+{
+    [SerializeField] private float transitionTime = 1f;
+    [SerializeField] private Animator transitionAnimator;
+
+    void Start()
+    {
+        if (transitionAnimator == null)
+            transitionAnimator = GetComponentInChildren<Animator>();
+    }
+
+    public IEnumerator SceneLoad(int sceneIndex)
+    {
+        transitionAnimator.SetTrigger("StartTransition");
+        yield return new WaitForSeconds(transitionTime);
+        SceneManager.LoadScene(sceneIndex);
+    }
+
+    public void Jugar()
+    {
+        Debug.Log("Iniciando el juego...");
+        StartCoroutine(SceneLoad(SceneManager.GetActiveScene().buildIndex + 1));
+    }
+
+    public void VolverAlMenu()
+    {
+        Debug.Log("Volviendo al menú principal...");
+        StartCoroutine(SceneLoad(0));
+    }
+
+    public void Opciones()
+    {
+        Debug.Log("Abriendo opciones...");
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Saliendo del juego...");
+        Application.Quit();
+    }
+}
